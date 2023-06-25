34f5dae7b9a995db413eab3fee76695e683a7ef9
Patrícia Bardini Arigoni <arigonipatricia@gmail.com>
2023-06-25 19:30:12 -0300

    otimiza a solução para matriz 4x4

--- matriz4x4.pl
+++ matriz4x4.pl
@@ -1,28 +1,26 @@
-%https://www.janko.at/Raetsel/Sudoku/Vergleich/001.a.htm
+% https://www.janko.at/Raetsel/Sudoku/Vergleich/001.a.htm
 
 :- use_module(library(clpfd)).
 
-n(1).
-n(2).
-n(3).
-n(4).
-
-todosDiferentes([]).
-todosDiferentes([H|T]) :- not(member(H,T)), todosDiferentes(T).
-
-completa([X1, X2, X3, X4]) :- n(X1), n(X2), n(X3), n(X4),
-                              todosDiferentes([X1, X2, X3, X4]).
+% predicado para exibir a matriz de solução linha por linha
+exibirMatriz([]).
+exibirMatriz([L|Ls]) :-
+    write(L), nl,
+    exibirMatriz(Ls).
 
 solucao(MatrizSolucao) :-
-    MatrizSolucao = tabuleiro([
-                                [X11, X12,      X13, X14],
-                                [X21, X22,      X23, X24],
+    MatrizSolucao = [[X11, X12,      X13, X14],
+                     [X21, X22,      X23, X24],
 
-                                [X31, X32,      X33, X34],
-                                [X41, X42,      X43, X44]
-                            ]),
+                     [X31, X32,      X33, X34],
+                     [X41, X42,      X43, X44]],
 
-    %tabuleiro do jogo (para resolver outro tabuleiro, alterar somente os operadores)
+    % transforma a matriz em uma lista contendo todos os elementos da matriz
+    flatten(MatrizSolucao, Elementos),
+    % elementos assumem valores de 1 a 4
+    Elementos ins 1..4,
+
+    % tabuleiro do jogo (para resolver outro tabuleiro, alterar somente os operadores)
     X11 #< X12,                 X13 #< X14,
     X11 #> X21, X12 #< X22,     X13 #< X23, X14 #> X24,
     X21 #< X22,                 X23 #> X24,
@@ -31,20 +29,18 @@ solucao(MatrizSolucao) :-
     X31 #> X41, X32 #< X42,     X33 #< X43, X34 #> X44,
     X41 #> X42,                 X43 #> X44,
 
-    %completa linhas
-    completa([X11, X12, X13, X14]),
-    completa([X21, X22, X23, X24]),
-    completa([X31, X32, X33, X34]),
-    completa([X41, X42, X43, X44]),
+    % verifica se há algum elemento repetido nas regiões 
+    all_different([X11, X12, X21, X22]),
+    all_different([X31, X32, X41, X42]),
+    all_different([X13, X14, X23, X24]),
+    all_different([X33, X34, X43, X44]),
+    
+    % verifica se há algum elemento repetido nas linhas
+    maplist(all_different, MatrizSolucao),
 
-    %completa colunas
-    completa([X11, X21, X31, X41]),
-    completa([X12, X22, X32, X42]),
-    completa([X13, X23, X33, X43]),
-    completa([X14, X24, X34, X44]),
+    % verifica se há algum elemento repetido nas colunas
+    transpose(MatrizSolucao, Transposta),
+    maplist(all_different, Transposta),
 
-    %completa regiões
-    completa([X11, X12, X21, X22]),
-    completa([X31, X32, X41, X42]),
-    completa([X13, X14, X23, X24]),
-    completa([X33, X34, X43, X44]).
+    % busca de soluções
+    label(Elementos).
